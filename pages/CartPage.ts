5d3755de631bc2965801de829e99b1d0a51b8a51
Tamas Andras Peter <tamas.andras@thestockmine.com>
2026-06-30 17:01:34 +0200

    update on Test 2

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -4,20 +4,34 @@ import { Endpoints, SauceDemoUtils } from '../utils/SauceDemoUtils.js';
 export class CartPage {
     private readonly page: Page;
     private readonly checkoutButton: Locator;
+    readonly itemsInCart: Locator; // Remove private
+    readonly cartBadge: Locator;
+
 
     constructor(page: Page) {
         this.page = page;
         this.checkoutButton = page.locator('#checkout');
+        this.itemsInCart = page.locator('[data-test="inventory-item"]');
+        this.cartBadge = page.locator('[data-test="shopping-cart-badge"]');
     }
 
     async clickOnCheckoutButton() {
         await this.checkoutButton.click();
     }
 
-
     async navigate() {
         await this.page.goto(SauceDemoUtils.buildUrl(Endpoints.CART));
         await this.page.waitForURL(SauceDemoUtils.buildUrl(Endpoints.CART));
+    }
+
+    async numberOfItemsInChart() {
+        const items = await this.itemsInCart.all();
+        return items.length;
+    }
+
+    async removeItemByName(itemName: string) {
+        const itemCard = this.itemsInCart.filter({ hasText: itemName });
+        await itemCard.getByRole('button', { name: 'Remove' }).click();
     }
 
 
---
+++ tests/modifyCart.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage.js';
+import { InventoryPage } from '../pages/InventoryPage.js';
+import { CartPage } from '../pages/CartPage.js';
+import { Endpoints, SauceDemoUtils } from '../utils/SauceDemoUtils.js';
+
+
+let loginPage: LoginPage;
+let inventoryPage: InventoryPage;
+let cartPage: CartPage;
+
+test.beforeEach('Open SauceDemo Site', async ({ page }) => {
+    loginPage = new LoginPage(page);
+    inventoryPage = new InventoryPage(page);
+    cartPage = new CartPage(page);
+    await loginPage.navigate();
+});
+
+test.describe('Modifying Chart At SauceDemo', () => {
+
+    test('TEST-02 - Modifying Chart At Saucedemo', async () => {
+        await loginPage.login();
+        await expect(loginPage.getPage()).toHaveURL(SauceDemoUtils.buildUrl(Endpoints.INVENTORY));
+
+        // Act 
+        await inventoryPage.addItemToCartByName("Sauce Labs Backpack");
+        await inventoryPage.addItemToCartByName("Sauce Labs Bolt T-Shirt");
+        await inventoryPage.addItemToCartByName("Sauce Labs Fleece Jacket");
+        await inventoryPage.goToCart();
+
+        // Assertions before deletion
+        await expect(loginPage.getPage()).toHaveURL(SauceDemoUtils.buildUrl(Endpoints.CART));
+        await expect(cartPage.itemsInCart).toHaveCount(3);
+        await expect(cartPage.cartBadge).toHaveText('3');
+
+        // Act - Remove an item
+        await cartPage.removeItemByName("Sauce Labs Backpack");
+
+        // Assertions after deletion
+        await expect(cartPage.itemsInCart).toHaveCount(2);
+        await expect(cartPage.cartBadge).toHaveText('2');
+
+        // Assert that the specified remaining items remain untouched!
+        await expect(cartPage.itemsInCart.filter({ hasText: "Sauce Labs Bolt T-Shirt" })).toBeVisible();
+        await expect(cartPage.itemsInCart.filter({ hasText: "Sauce Labs Fleece Jacket" })).toBeVisible()
+
+    });
+
+});
